613ef4a20bade9f749c23ea11e25ebdba8bd3997
Harry Oliver <someonecancode@yahoo.co.uk>
2016-01-19 16:59:56 +0000

    Add login form

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  root 'pictures#index'
+  get '/login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+  
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
+  root 'pictures#index'
+
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
